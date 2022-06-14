9297be6da3f83b9dfe197c04b898e8c7104e147a
Denys Niestierov <nercifal@gmail.com>
2022-06-14 22:09:33 +0300

    First commit - ready to work

---
+++ NWeatherApp/Controllers/WelcomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  WelcomeViewController.swift
+//  NWeatherApp
+//
+//  Created by Denys Niestierov on 14.06.2022.
+//
+
+import UIKit
+import FBSDKLoginKit
+import GoogleSignIn
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet var backgroundImage: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        randomBackgroundImage()
+    }
+    
+    //MARK: Change background image to random
+    func randomBackgroundImage() {
+            let randomNumber = Int.random(in: 0..<6)
+            
+            switch(randomNumber) {
+            case 0:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.deepNight)
+            case 1:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.fallingStar)
+            case 2:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.greenLeaves)
+            case 3:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.riverMountain)
+            case 4:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.sunsetField)
+            case 5:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.twilightMoon)
+            default:
+                backgroundImage.image = UIImage(named: Constants.BackgroundImage.greenLeaves)
+            }
+            
+            backgroundImage.alpha = 0.5
+        }
+
+}
---
+++ NWeatherApp/SupportingFiles/Constants.swift
@@ -0,0 +1,26 @@
+//
+//  Constants.swift
+//  NWeatherApp
+//
+//  Created by Denys Niestierov on 14.06.2022.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct BackgroundImage {
+        static let twilightMoon = "twilightMoonBackround"
+        static let sunsetField = "sunsetFieldBackround"
+        static let riverMountain = "riverMountainBackground"
+        static let greenLeaves = "greenLeavesBackground"
+        static let fallingStar = "fallingStarBackground"
+        static let deepNight = "deepNightBackground"
+    }
+    
+    struct API {
+        static let googleMapsApi = "AIzaSyAGDORMYoad9KIzrDiA-DZ5Ffc-uVo11xg"
+        
+    }
+    
+}
